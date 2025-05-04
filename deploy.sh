ed5bbbe6dccd833d091ddaa9f16a3ec060e0020a
Siyi Zhao <52352980+SiyiZhao@users.noreply.github.com>
2025-05-04 21:19:45 +0800

    脚本部署到GitHub Pages
    参考@hxywd666的评论（教程页 https://theme-plume.vuejs.press/guide/deployment/ ）

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+ 
+# 忽略错误
+set -e  #有错误抛出错误
+ 
+# 构建
+npm run docs:build  #然后执行打包命令
+ 
+# 进入待发布的目录
+cd docs/.vuepress/dist  #进到dist目录
+ 
+git init  #执行这些git命令
+git add -A
+git commit -m 'deploy'
+ 
+# 上传仓库，这里改成你自己的仓库
+git push -f git@github.com:SiyiZhao/SolarZip.git master:gh-pages
+ 
+cd -
+ 
+rm -rf docs/.vuepress/dist  #删除dist文件夹

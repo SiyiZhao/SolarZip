d21f164bc28f78ca9346f4c1d4c9f9d0c3890174
Siyi Zhao <52352980+SiyiZhao@users.noreply.github.com>
2025-05-04 21:58:15 +0800

    plume.configs

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -6,12 +6,12 @@ import { enNotes, zhNotes } from './notes'
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+  logo: 'images/logo.jpeg',
 
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: '/' },
+    { icon: 'github', link: 'https://github.com/SiyiZhao/' },
   ],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
@@ -21,17 +21,18 @@ export default defineThemeConfig({
    * 文章版权信息
    * @see https://theme-plume.vuejs.press/guide/features/copyright/
    */
-  // copyright: true,
+  copyright: true,
 
-  // prevPage: true,   // 是否启用上一页链接
-  // nextPage: true,   // 是否启用下一页链接
-  // createTime: true, // 是否显示文章创建时间
+
+  prevPage: true,   // 是否启用上一页链接
+  nextPage: true,   // 是否启用下一页链接
+  createTime: true, // 是否显示文章创建时间
 
   /* 站点页脚 */
-  // footer: {
-  //   message: 'Power by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
-  //   copyright: '',
-  // },
+  footer: {
+    message: 'Power by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
+    copyright: 'Copyright © 2025-present Siyi Zhao',
+  },
 
   /* 过渡动画 @see https://theme-plume.vuejs.press/config/basic/#transition */
   // transition: {
@@ -46,11 +47,11 @@ export default defineThemeConfig({
        * @see https://theme-plume.vuejs.press/config/basic/#profile
        */
       profile: {
-        avatar: 'https://theme-plume.vuejs.press/plume.png',
+        avatar: 'images/logo.jpeg',
         name: 'SolarZip',
         description: 'Zip the knowledge and thoughts in the Solar System! ',
-        // circle: true,
-        // location: '',
+        circle: true,
+        location: 'Beijing, China',
         // organization: '',
       },
 
@@ -73,11 +74,11 @@ export default defineThemeConfig({
        * @see https://theme-plume.vuejs.press/config/basic/#profile
        */
       profile: {
-        avatar: 'https://theme-plume.vuejs.press/plume.png',
+        avatar: 'images/logo.jpeg',
         name: 'SolarZip',
         description: 'Zip the knowledge and thoughts in the Solar System! ',
-        // circle: true,
-        // location: '',
+        circle: true,
+        location: 'Beijing, China',
         // organization: '',
       },
 
